7b96105808afc67bd6b5f915eb8c6348f25578f8
Skyler Ficklin <skylerficklin@gmail.com>
2026-01-26 02:15:05 -0500

    Initial Commit

---
+++ LittleEyedropperGuy/LittleEyedropperGuyApp.swift
@@ -0,0 +1,17 @@
+//
+//  LittleEyedropperGuyApp.swift
+//  LittleEyedropperGuy
+//
+//  Created by Skyler Ficklin on 1/26/26.
+//
+
+import SwiftUI
+
+@main
+struct LittleEyedropperGuyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
